b2db8d4e0c1018bfa5d23693f82103f0232dfa6f
Vítězslav Krejčí <krejciv22@spstrutnov.cz>
2023-02-22 13:17:15 +0100

    Dokončení pozadí

--- Pong.py
+++ Pong.py
@@ -4,6 +4,7 @@ import pygame
 import math
 pygame.init()
 
+
 ROZLISENI_Y = 1017
 ROZLISENI_X = 1920
 FPS = 60
@@ -27,6 +28,14 @@ pozadi = pygame.transform.scale(pozadi,(1920,1017,))
 micek = pygame.image.load('micek spravny.png')
 micek = pygame.transform.scale(micek,(100,100,))
 
+platforma = pygame.image.load('platforma.png')
+platforma = pygame.transform.scale(platforma,(40,300,))
+
+platforma2 = pygame.image.load('platforma2.png')
+platforma2 = pygame.transform.scale(platforma,(700,40,))
+
+
+
 
 while True:
     for udalost in pygame.event.get():
@@ -36,5 +45,13 @@ while True:
     
     okno.blit(pozadi,(0,0))
     okno.blit(micek,(902,510))
-
+    okno.blit(platforma,(0,358))
+    okno.blit(platforma,(1880,358))
+    okno.blit(platforma2,(606,3))
+    okno.blit(platforma2,(606,965))
+   
+   
+   
+   
+   
     pygame.display.update()
